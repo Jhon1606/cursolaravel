4fe26a7598f0a31ef3a89e8bd50c720b3abbf0ba
Jhon1606 <barrosjhon936@gmail.com>
2022-01-11 11:49:47 -0500

    Formulario del curso de laravel, crear base de datos llamado todos

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('content')
+    <div class="container w-25 border p-4 mt-4">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Título de la tarea</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <button type="submit" class="btn btn-primary">Crear nueva tarea</button>
+        </form>
+    </div>
+@endsection
